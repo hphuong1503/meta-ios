1608854fea17547f1de02b9608e5bed70de8b6ab
hphuong1503 <hphuong1503@outlook.com>
2024-04-02 13:31:27 +0700

    first commit

---
+++ LittleLemonApp/LittleLemonApp/LittleLemonApp.swift
@@ -0,0 +1,16 @@
+//
+//  LittleLemonAppApp.swift
+//  LittleLemonApp
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    let persistenceController = PersistenceController.shared
+    
+    var body: some Scene {
+        WindowGroup {
+            Onboarding().environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
